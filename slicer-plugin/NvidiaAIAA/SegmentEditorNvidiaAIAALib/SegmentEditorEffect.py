ed75b55fad747275350a2274c57ee849c8833cee
Andras Lasso <lasso@cs.queensu.ca>
2022-05-27 14:26:18 -0400

    Update default Slicer segmentation server address (#100)
    The default NVIDIA AIAA segmentation server for Slicer was moved to a new location, therefore we need to update the server address.

--- slicer-plugin/NvidiaAIAA/SegmentEditorNvidiaAIAALib/SegmentEditorEffect.py
+++ slicer-plugin/NvidiaAIAA/SegmentEditorNvidiaAIAALib/SegmentEditorEffect.py
@@ -84,7 +84,7 @@ class SegmentEditorEffect(AbstractScriptedSegmentEditorEffect):
         serverUrl = self.ui.serverComboBox.currentText
         if not serverUrl:
             # Default Slicer AIAA server
-            serverUrl = "http://perklabseg.asuscomm.com:5000"
+            serverUrl = "http://perklabseg.cs.queensu.ca:5000"
         return serverUrl
 
     def setupOptionsFrame(self):
